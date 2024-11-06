bd02002c20bfdc29fa83b4f453778767d0088a0c
cruzte4 <162765665+cruzte4@users.noreply.github.com>
2024-11-06 22:14:40 +0700

    a

---
+++ TUBES-WEBPRO-FaridHanafi_1302223043/Tubes_LihatProfil/script.js
@@ -0,0 +1,73 @@
+document.getElementById("tambahringkasan").addEventListener("click", function() {
+  document.getElementById("popup").classList.add("active");
+});
+
+document.getElementById("closePopup").addEventListener("click", function() {
+  document.getElementById("popup").classList.remove("active");
+});
+
+document.getElementById("tambahpekerjaan").addEventListener("click", function() {
+  document.getElementById("popuppekerjaan").classList.add("active");
+});
+
+document.getElementById("closePopuppekerjaan").addEventListener("click", function() {
+  document.getElementById("popuppekerjaan").classList.remove("active");
+});
+
+document.getElementById("simpanbutton").addEventListener("click", function() {
+  var summaryText = document.getElementById("summary").value;
+
+  if (summaryText.trim() === "") {
+      alert("Ringkasan tidak boleh kosong.");
+      return;
+  }
+
+  var summarySection = document.getElementById("ringkasan").parentElement;
+  var newSummary = document.createElement("p");
+  var content = document.getElementById("ringkasan")
+  newSummary.textContent = summaryText;
+
+  var existingSummary = summarySection.querySelector("p.summary-text");
+  if (existingSummary) {
+      existingSummary.textContent = summaryText;
+  } else {
+    content.innerHTML = "";
+      newSummary.classList.add("summary-text");
+      summarySection.insertBefore(newSummary, summarySection.querySelector(".add-button"));
+  }
+
+  document.getElementById("popup").classList.remove("active");
+  document.getElementById("summary").value = ""; 
+});
+
+document.getElementById("simpanpekerjaanbutton").addEventListener("click", function() {
+  var summaryKursusText = document.getElementById("summarykursus").value;
+  var summaryLembagaText = document.getElementById("summarylembaga").value;
+  var summaryTahunText = document.getElementById("summarytahun").value;
+  var summaryPoinText = document.getElementById("summarypoin").value;
+
+  if (summaryKursusText.trim() === "" || summaryLembagaText.trim() === "") {
+      alert("Ringkasan tidak boleh kosong.");
+      return;
+  }
+
+  document.getElementById("kursus").innerHTML = `<strong>${summaryKursusText}</strong>`;
+  document.getElementById("lembaga").innerHTML = summaryLembagaText;
+  document.getElementById("tahun").innerHTML = summaryTahunText;
+  document.getElementById("poin").innerHTML = summaryPoinText;
+
+  
+  var existingSummary = summarySection.querySelector("p.summary-text");
+  if (existingSummary) {
+      existingSummary.innerHTML = summaryText; 
+  } else {
+      var newSummary = document.createElement("p");
+      newSummary.innerHTML = summaryText; 
+      newSummary.classList.add("summary-text");
+      content.innerHTML = ""; 
+      summarySection.insertBefore(newSummary, summarySection.querySelector(".add-button"));
+  }
+  document.getElementById("popup").classList.remove("active");
+  document.getElementById("summary").value = ""; 
+});
+
